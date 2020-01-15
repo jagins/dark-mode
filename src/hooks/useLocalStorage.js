e0ac520fc3b4bbba34bba69034b81d58821fa2d2
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-01-15 14:43:02 -0600

    step 1 complete setting up functionality of the useLocalStorage hook

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,33 @@
+import {useState} from 'react';
+
+//useLocalStorage is a custom hook
+const useLocalStorage = () =>
+{
+    //setup state using useState and passing in an annymous function
+    const [storedValue, setStoredValue] = useState((key, initialValue) =>
+    {
+        //getting what is currently in localStorage
+        const item = window.localStorage.getItem(key);
+
+        //if something is in local storage and return what's in there or else just return the inital value that was passed in
+        if(item)
+        {
+            return JSON.parse(item);
+        }
+        else
+        {
+            return initialValue;
+        }
+    });
+
+    const setValue = value =>
+    {
+        //save state
+        setStoredValue(value);
+        
+        //saves to local storage as a string
+        window.localStorage.setItem(key, JSON.stringify(value));
+    }
+
+    return [storedValue, setValue];
+}
